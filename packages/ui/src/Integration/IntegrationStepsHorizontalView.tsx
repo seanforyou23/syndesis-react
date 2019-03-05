05e8d37dc38649ee4680b7bb1c34ddb901f31635
kahboom <rachelyordan@gmail.com>
2019-03-05 13:18:07 -0500

    Set up grid system again

--- packages/ui/src/Integration/IntegrationStepsHorizontalView.tsx
+++ packages/ui/src/Integration/IntegrationStepsHorizontalView.tsx
@@ -10,32 +10,42 @@ export class IntegrationStepsHorizontalView extends React.Component<
 > {
   public render() {
     return (
-      <Grid fluid={true}>
-        {this.props.steps &&
-          this.props.steps.map((opt: any) => (
+      <>
+        <Grid fluid={true}>
+          <Grid.Row className="show-grid">
+            <Grid.Col xs={6} md={4}>
+              {this.props.steps && this.props.steps[0] ? (
+                <Icon name={'external-link'} />
+              ) : null}
+            </Grid.Col>
+            {this.props.steps &&
+              this.props.steps.map((opt: any) => (
+                <>
+                  <Grid.Col xs={6} md={4}>
+                    <Icon name={'angle-right'} />
+                  </Grid.Col>
+                  <Grid.Col xsHidden={true} md={4}>
+                    <span>
+                      <p key={opt.id}>{opt.name}</p>
+                    </span>
+                  </Grid.Col>
+                </>
+              ))}
+          </Grid.Row>
+        </Grid>
+        <Grid fluid={true}>
+          {this.props.steps ? (
             <Grid.Row className="show-grid">
               <Grid.Col xs={2} md={2}>
-                <Icon name={'external-link'} />
+                {<span>Step:</span>}
               </Grid.Col>
               <Grid.Col xs={10} md={10}>
-                <span>
-                  <p key={opt.id}>{opt.name}</p>
-                </span>
+                {this.props.steps ? <span>Goodbye</span> : null}
               </Grid.Col>
             </Grid.Row>
-          ))}
-
-        {this.props.steps ? (
-          <Grid.Row className="show-grid">
-            <Grid.Col xs={2} md={2}>
-              {<span>Step:</span>}
-            </Grid.Col>
-            <Grid.Col xs={10} md={10}>
-              {this.props.steps ? <span>Goodbye</span> : null}
-            </Grid.Col>
-          </Grid.Row>
-        ) : null}
-      </Grid>
+          ) : null}
+        </Grid>
+      </>
     );
   }
 }
